551d26295daa35acfb37809cc84b2846a129d08b
expectopatronum <veroamilbe@gmail.com>
2016-10-30 15:25:18 +0100

    adds help for package

---
+++ R/austRia.R
@@ -0,0 +1,7 @@
+#' austRia: A package containing data about Austria
+#'
+#' This package contains data about Austria collected from different sources.
+#' 
+#' @docType package
+#' @name austRia
+NULL
