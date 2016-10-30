9dfb02ea2d849cd209cf32219dc8e7c0b1dc8075
expectopatronum <veroamilbe@gmail.com>
2016-10-30 13:55:00 +0100

    finish preparation of bpw data

--- prep_data/bpw16/read_transform_data.R
+++ prep_data/bpw16/read_transform_data.R
@@ -11,23 +11,30 @@ read_gkz_info <- function(filepath) {
 prep_bpw16 <- function(filepath, gkz) {
   library(xlsx)
   filecontent <- read.xlsx2(filepath, sheetIndex = 1, header = FALSE, stringsAsFactors=FALSE) # it has a header but unfortunately 2 rows
-  #filecontent[] <- lapply(filecontent, as.character)
   
   # split into header and content
+  keep <- (1:ncol(filecontent))[-seq(8, ncol(filecontent), 2)]
+  filecontent <- filecontent[,keep]
   header <- filecontent[1:2,]
   filecontent <- filecontent[-(1:2),]
   
   # process header - difference between files is number of candidates
-  ncand <- (ncol(header)-7+1)/2
-  candidate_header <- header[1, 7:ncol(header)]
-  candidate_header[seq(2, ncol(candidate_header), 2)] <- paste0(candidate_header[1, seq(1, ncol(candidate_header), 2)], "_Perc")
+  #ncand <- (ncol(header)-7+1)/2
+  candidate_header <- tolower(header[1, 7:ncol(header)])
+  #candidate_header[seq(2, ncol(candidate_header), 2)] <- paste0(candidate_header[1, seq(1, ncol(candidate_header), 2)], "_Perc")
   
-  col_names <- c(as.character(header[1, 1:3]), as.character(header[2, 4:6]), candidate_header)
-  print(col_names)
+  col_names <- c(c("gkz", "gebietsname", "wahlberechtigte", "abgegebene", "ungueltige", "gueltige"), candidate_header)
   colnames(filecontent) <- col_names
-
+  
+  filecontent[,"wahlberechtigte"] <- as.numeric(filecontent[,"wahlberechtigte"])
+  filecontent[,"abgegebene"] <- as.numeric(filecontent[,"abgegebene"])
+  filecontent[,"ungueltige"] <- as.numeric(filecontent[,"ungueltige"])
+  for (cand in candidate_header) {
+    filecontent[, cand] <- as.numeric(filecontent[,cand])
+  }
+  
   filecontent <- filecontent[!(substr(filecontent[,1], start = 5, 6) %in% c("00")) &
-                               !(substr(filecontent[,1], start = 3, 6) %in% c("0099")), ]  
+                             !(substr(filecontent[,1], start = 3, 6) %in% c("0099")), ]  
 
   return(filecontent)
 }
@@ -51,13 +58,6 @@ print(head(bpw_wg2))
 sum(as.integer(bpw_wg1[,3])) == 6382507
 sum(as.integer(bpw_wg1[,4])) == 4371825
 
-# neu downloaden
-# deutsches + englisches README schreiben
-# mit Pfaden
-# change column names
-# save as RDS
-# "package" erstellen
+saveRDS(bpw_wg1, file="data/bpw_wg1.RDS")
+saveRDS(bpw_wg2, file="data/bpw_wg2.RDS")
 
-# gkz: https://www.statistik.at/web_de/klassifikationen/regionale_gliederungen/gemeinden/index.html
-# http://www.bmi.gv.at/cms/BMI_wahlen/bundespraes/bpw_2016/Ergebnis.aspx
-# http://www.bmi.gv.at/cms/BMI_wahlen/bundespraes/bpw_2016/Ergebnis_2WG.aspx
